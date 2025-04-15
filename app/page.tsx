edc3985a106c4c9374f898493b009874ebb3cbc8
bosco0808 <bosco0808@gmail.com>
2025-04-15 12:10:17 +0800

    finished main page and changed metadata 🚀

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Eight8Chat</h1>
+      <Link
+        href="/signup"
+        className={`btn btn-primary btn-lg ${styles.signupbutton} grid-item`}
+      >
+        Sign Up
+      </Link>
+      <Link
+        href="/signin"
+        className={`btn btn-secondary btn-lg ${styles.signinbutton} grid-item`}
+      >
+        Sign In
+      </Link>
+    </div>
+  );
 }
