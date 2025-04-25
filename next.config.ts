7c72e33f83b96c834aa880e0191a0cfc95d3d5a2
bosco0808 <bosco0808@gmail.com>
2025-04-25 19:44:28 +0800

    revert

--- next.config.ts
+++ next.config.ts
@@ -4,24 +4,6 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async webpack(config) {
-    const sourcePath = path.join(
-      process.cwd(),
-      "node_modules/.prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node"
-    );
-    const destinationPath = path.join(
-      process.cwd(),
-      "generated/prisma/libquery_engine-rhel-openssl-3.0.x.so.node"
-    );
-
-    // Ensure the destination directory exists
-    await fs.mkdir(path.dirname(destinationPath), { recursive: true });
-
-    // Copy the file
-    await fs.copyFile(sourcePath, destinationPath);
-
-    return config;
-  },
 };
 
 export default nextConfig;
