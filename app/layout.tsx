4b0b60a1b67347c6ab029480d413e2d32f3040b4
bosco0808 <bosco0808@gmail.com>
2025-04-27 13:03:44 +0800

    edited metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
 import Navbar from "./Navbar";
 
 export const metadata: Metadata = {
-  title: "Eight8Chat",
-  description: "A chat app for making friends and chatting with them",
+  title: "Eight8Chat - Your Ultimate Web Chat App",
+  description:
+    "Discover eight8chat, the ultimate web chat app for seamless communication! Connect with friends and family through instant messaging, group chats, and enhanced privacy settings. Enjoy a user-friendly interface and customizable themes for a personalized chatting experience. Join eight8chat today and stay connected effortlessly! Eight8chat is still under heavy development, so expect some bugs and missing features.",
 };
 
 export default function RootLayout({
