2caa043c2370221380954430cd5f2da916ba26e0
bosco0808 <bosco0808@gmail.com>
2025-05-14 20:40:03 +0800

    removed commented code blocks in ChatMessages.tsx

--- app/chat/[id]/ChatMessages.tsx
+++ app/chat/[id]/ChatMessages.tsx
@@ -53,65 +53,6 @@ const ChatMessages = ({
     }
     initES();
   }, [userId, friendId]);
-  /*
-  useEffect(() => {
-    async function subscribe(userId: number, friendId: number) {
-      const unParsedResults = await getMessages(userId, friendId);
-      const { result, messages, errMessage } = JSON.parse(unParsedResults);
-      switch (result) {
-        case "ERR_NO_SENDERID_OR_RECEIVERID":
-          setIsLoading(false);
-          setIsError(true);
-          setErrMessage("Sender ID or Receiver ID is missing.");
-          break;
-        case "ERR_SAME_USER":
-          setIsLoading(false);
-          setIsError(true);
-          setErrMessage("You cannot send a message to yourself.");
-          break;
-        case "ERR_INVALID_SENDERID_OR_RECEIVERID":
-          setIsLoading(false);
-          setIsError(true);
-          setErrMessage("Invalid Sender ID or Receiver ID.");
-          break;
-        case "ERR_NOT_FRIENDS":
-          setIsLoading(false);
-          setIsError(true);
-          setErrMessage("You are not friends with this user.");
-          break;
-        case "ERR_NOT_AUTHENTICATED":
-          setIsLoading(false);
-          setIsError(true);
-          setErrMessage("You are not authenticated.");
-          break;
-        case "SUCCESS":
-          setIsLoading(false);
-          setMessages(messages);
-          setResult(result);
-          setIsError(false);
-          setErrMessage("");
-          break;
-        case "ERR":
-          setIsLoading(false);
-          setIsError(true);
-          console.log(errMessage);
-          setErrMessage("An error occurred while receiving the messages.");
-          break;
-        default:
-          setIsLoading(false);
-          setIsError(true);
-          setErrMessage("An error occurred while receiving the messages.");
-          console.log(errMessage);
-          break;
-      }
-    }
-    setInterval(() => {
-      subscribe(userId, friendId);
-    }, 1000); // short polling replacing long polling(for now)
-    // TODO: replace with long polling that actually scales and not recursively spam requests to server
-    subscribe(userId, friendId); // Will recursively call itself for new messages(long polling)
-  }, []);
-  */
   return (
     <div style={{ overflowY: "scroll", height: "50vh" }} ref={scrollableRef}>
       {messages && messages.length > 0 ? (
