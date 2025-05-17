22cce468e82179a9bce5e78df108f3fa95f3768b
bosco0808 <bosco0808@gmail.com>
2025-05-17 15:56:23 +0800

    update: persistent color mode in localStorage

--- app/ColorThemeToggler.tsx
+++ app/ColorThemeToggler.tsx
@@ -2,9 +2,17 @@
 import { useAtom } from "jotai";
 import { isDarkModeAtom } from "@/atoms";
 import styles from "./ColorThemeToggler.module.scss";
+import { useEffect } from "react";
 
 export default function ColorThemeToggler() {
   const [isDarkMode, setIsDarkMode] = useAtom(isDarkModeAtom);
+  useEffect(() => {
+    const storedIsDarkMode = localStorage.getItem("mode");
+    storedIsDarkMode === "light" ? setIsDarkMode(false) : setIsDarkMode(true);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("mode", isDarkMode ? "dark" : "light");
+  }, [isDarkMode]);
   return (
     <button
       onClick={() => setIsDarkMode((v) => !v)}
